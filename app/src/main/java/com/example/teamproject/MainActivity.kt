9b31b536b765bb6178744bceeaba6a2fc7a5ec1c
chxisoei <minseo040727@naver.com>
2024-12-20 05:30:39 +0900

    feat: Implement MainActivity

--- app/src/main/java/com/example/teamproject/MainActivity.kt
+++ app/src/main/java/com/example/teamproject/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.teamproject
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Html
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,7 +14,21 @@ import com.example.teamproject.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.statusBarColor = Color.parseColor("#B8660E")
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        // 툴바 초기화
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.title=""
+
+        binding.chatbtn.setOnClickListener {
+            val intent = Intent(this, ChatActivity::class.java)
+            startActivity(intent)
+        }
+        binding.mapbtn.setOnClickListener {
+            val intent = Intent(this, MapActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
