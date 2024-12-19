2cb2d8c4e711fd8b60c2d08dafc5bdae56d4ffdd
chxisoei <minseo040727@naver.com>
2024-12-19 23:12:40 +0900

    chore: update okhttp3 logging-interceptor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
 
 dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.9.3") // OkHttp 라이브러리
-    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
